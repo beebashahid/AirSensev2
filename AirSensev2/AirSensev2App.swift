b61f84521b859a616e23d5ebc2152ece7c797371
beebashahid <areebashahid1995@gmail.com>
2023-04-02 17:40:30 +0400

    Initial Commit

---
+++ AirSensev2/AirSensev2App.swift
@@ -0,0 +1,17 @@
+//
+//  AirSensev2App.swift
+//  AirSensev2
+//
+//  Created by areeba shahid on 02/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirSensev2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
